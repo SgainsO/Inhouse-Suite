6af5ae7d340ec3e4e2211a5ffa4c98ad1cc6d2ab
SgainsO <jptjasc@gmail.com>
2026-01-07 13:19:14 -0500

    changed some of the theme to be more aligned with DGG colors

--- Application/app/layout.tsx
+++ Application/app/layout.tsx
@@ -2,9 +2,27 @@ import type { Metadata } from "next";
 import '@mantine/core/styles.css';
 import { Geist, Geist_Mono } from "next/font/google";
 import NavbarSimple from './components/Navbar/Navbar';
-import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
+import { ColorSchemeScript, MantineProvider, mantineHtmlProps, createTheme, MantineColorsTuple } from '@mantine/core';
 
+const myColor: MantineColorsTuple = [
+  '#ecefff',
+  '#d5dafb',
+  '#a9b1f1',
+  '#7a87e9',
+  '#5362e1',
+  '#3a4bdd',
+  '#2c40dc',
+  '#1f32c4',
+  '#182cb0',
+  '#0a259c'
+];
 
+const theme = createTheme({
+  colors: {
+    brand: myColor,
+  },
+  primaryColor: 'brand',
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +51,7 @@ export default function RootLayout({
           </head>
 
           <body>
-            <MantineProvider>
+            <MantineProvider theme={theme}>
             <div style={{ display: 'flex' }}>
               <NavbarSimple />
               <main style={{ flex: 1, padding: '20px' }}>
